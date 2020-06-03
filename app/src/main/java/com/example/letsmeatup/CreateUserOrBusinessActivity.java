4e882c5153d121de42c98b2ca6b8c83e5ed8a503
cammylim <64000790+cammylim@users.noreply.github.com>
2020-06-03 17:34:31 +0800

    Created create user or business XML. Made intent from main to signuporlogin.

---
+++ app/src/main/java/com/example/letsmeatup/CreateUserOrBusinessActivity.java
@@ -0,0 +1,14 @@
+package com.example.letsmeatup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateUserOrBusinessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_user_or_business);
+    }
+}
