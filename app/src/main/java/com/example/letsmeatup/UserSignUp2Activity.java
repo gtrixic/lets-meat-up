b0a8901dc2e307355c0764e2e11d23d26c1a4875
Zhe Wei Benjamin Lai <s10193638@connect.np.edu.sg>
2020-06-05 19:40:20 +0800

    partial completion of UserSignUp2Activity

--- app/src/main/java/com/example/letsmeatup/UserSignUp2Activity.java
+++ app/src/main/java/com/example/letsmeatup/UserSignUp2Activity.java
@@ -1,8 +1,14 @@
 package com.example.letsmeatup;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class UserSignUp2Activity extends AppCompatActivity {
 
@@ -10,5 +16,26 @@ public class UserSignUp2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_sign_up2);
+        ImageButton backButton = findViewById(R.id.backArrow);
+        ImageButton nextButton = findViewById(R.id.nextArrow);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UserSignUp2Activity.this,UserSignUp3Activity.class);
+                startActivity(intent);
+            }
+        });
+        AlertDialog.Builder b =new AlertDialog.Builder(this);
+        b.setTitle("WARNING!");
+        b.setMessage("This will bring you to the login page, are you sure?");
+        b.setCancelable(false);
+        b.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(UserSignUp2Activity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        })
+
     }
 }
