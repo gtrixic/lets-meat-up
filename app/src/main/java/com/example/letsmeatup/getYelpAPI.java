fd03c3885d4ac6d6bf97628d830df06905ef23b7
Zhe Wei Benjamin Lai <s10193638@connect.np.edu.sg>
2020-06-06 18:36:47 +0800

    yelp api done

--- app/src/main/java/com/example/letsmeatup/getYelpAPI.java
+++ app/src/main/java/com/example/letsmeatup/getYelpAPI.java
@@ -1,37 +1,57 @@
 package com.example.letsmeatup;
 
-import org.apache.commons.*;
-
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+
 
 public class getYelpAPI{
-    private static final String API_HOST = "api.yelp.com";
-    private static final String YELP_SEARCHPATH = "/v3/businesses/search";
-    private static final String clientID = "i6TGYF0mi-FePzzS7cnqBg";
+    private static final String API_HOST = "https://api.yelp.com/v3/businesses/search?";
     private static final String APIKEY = "w0hqahBgHMnJVITtl7KmT78HaYTrC9FZrfEu153HfurXH9HI_p2kWxn31_ml-JoP1Rx3Th9qlSPiM968MlOCFEuyOgDhtrFFFbCWV2BVkufDSr27N_InYw63P0HbXnYx";
 
-    public String getYelpRestaurants(){
-        HttpURLConnection connection = null;
-        try{
-
-            URL url = new URL(API_HOST+YELP_SEARCHPATH);
-            connection = (HttpURLConnection)url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setRequestProperty("Authorization","Bearer "+APIKEY);
-            connection.setRequestProperty("term","delis");
-            connection.setRequestProperty("location","Singapore");
-            connection.setRequestProperty("categories","Food");
+    public String getYelpRestaurants(HashMap<String,String>params) throws IOException {
+        HttpURLConnection connection;
+        URL url = new URL(API_HOST+getPostDataString(params));
+        connection = (HttpURLConnection)url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Authorization","Bearer "+APIKEY);
 
 
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuffer result = new StringBuffer();
+
+        while ((inputLine = reader.readLine()) != null) {
+            result.append(inputLine);
         }
+        reader.close();
+        return result.toString();
+
+
+    }
+
+    private static String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException{
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for(Map.Entry<String, String> entry : params.entrySet()){
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        }
+
+        return result.toString();
     }
 
 
