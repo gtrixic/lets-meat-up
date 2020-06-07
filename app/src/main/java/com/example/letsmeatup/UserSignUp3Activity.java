c63bbd5b25c3adc63b97ee49fa355d3c08fade52
Gek Teng Cher <s10196753@connect.np.edu.sg>
2020-06-07 23:16:32 +0800

    edited xml padding for edit text created spinner for diet in User Sign Up 3

--- app/src/main/java/com/example/letsmeatup/UserSignUp3Activity.java
+++ app/src/main/java/com/example/letsmeatup/UserSignUp3Activity.java
@@ -4,15 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
-public class UserSignUp3Activity extends AppCompatActivity {
+public class UserSignUp3Activity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private String TAG = "Let's Meat Up! SignUpActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_sign_up3);
+
+        final Spinner diet = findViewById(R.id.diet);
+        ArrayAdapter dAdapter = ArrayAdapter.createFromResource(this, R.array.diet_array, R.layout.spinner_layout);
+        dAdapter.setDropDownViewResource(R.layout.spinner_dropdown_layout);
+        diet.setAdapter(dAdapter);
+        diet.setOnItemSelectedListener(this);
 
         Button createProfile;
 
@@ -24,5 +35,15 @@ public class UserSignUp3Activity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Log.v(TAG, parent.getSelectedItem().toString() + "selected.");
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
