56542f55374d53043948f09668cc56d84ed77664
Jia Wen Tan <s10196330@connect.np.edu.sg>
2020-07-17 23:11:16 +0800

    user profile - profile picture

--- app/src/main/java/com/example/letsmeatup/UserProfile.java
+++ app/src/main/java/com/example/letsmeatup/UserProfile.java
@@ -1,31 +1,54 @@
 package com.example.letsmeatup;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.FirebaseApiNotAvailableException;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class UserProfile extends AppCompatActivity {
     private static final String TAG = "Let's-Meat-Up";
     private String FILENAME = "UserProfile.java";
 
+    DatabaseReference databaseReference;
+    StorageReference storageReference;
+
+    ImageView pfp;
     TextView viewUsername;
     TextView name;
     TextView age;
     TextView gender;
     TextView dob;
     TextView allergies;
+    private FirebaseAuth mAuth;
     LMUDBHandler dbHandler = new LMUDBHandler(this,null,null,1);
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        pfp = findViewById(R.id.profilePic);
         viewUsername = findViewById(R.id.usernameView);
         name = findViewById(R.id.nameView);
         age = findViewById(R.id.ageView);
         gender = findViewById(R.id.genderView);
         dob = findViewById(R.id.dobView);
         allergies = findViewById(R.id.allergiesView);
+        mAuth = FirebaseAuth.getInstance();
+
+        //profile picture
+        storageReference = FirebaseStorage.getInstance().getReference("uploads");
+        FirebaseUser firebaseUser = mAuth.getCurrentUser();
+        databaseReference = FirebaseStorage.getInstance().getReference("Users").child(firebaseUser.getUid);
+
 
         viewUsername = dbHandler.getUserDetail(username);
     }
